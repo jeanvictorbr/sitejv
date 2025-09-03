c875e9524c94060a9c33708f76399725b02a5061
jeanvictorbr <jeanvictorbr@gmail.com>
2025-09-03 15:54:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -41,9 +41,9 @@ function App() {
         <Route
           path="/feedback"
           element={
-            <ProtectedRoute>
+            
               <FeedbackPage />
-            </ProtectedRoute>
+            
           }
         />
 
