cfabd9413f6156cedd017f45831507f82944ea67
jeanvictorbr <jeanvictorbr@gmail.com>
2025-09-04 02:41:16 -0300

    Create GradientBackground.tsx

---
+++ src/components/GradientBackground.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './GradientBackground.module.css';
+
+export function GradientBackground() {
+  return <div className={classes.gradientBackground}></div>;
+}
