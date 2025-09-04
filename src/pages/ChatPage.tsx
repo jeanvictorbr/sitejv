23a36a2d218a863362f66ef711f8dbdb702e0fcd
jeanvictorbr <jeanvictorbr@gmail.com>
2025-09-04 02:34:55 -0300

    Update ChatPage.tsx

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 // --- Ícones SVG ---
 const IconSparkles = (props: React.ComponentProps<'svg'>) => ( <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M12 3a6 6 0 0 0 9 9a9 9 0 1 1-9-9Z" /></svg> );
-const IconSend = (props: React.ComponentProps<'svg'>) => ( <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M10 14l11 -11" /><path d="M21 3l-6.5 18a.55 .55 0 0 1-1 0l-3.5-7l-7-3.5a.55 .55 0 0 1 0-1l18-6.5" /></svg> );
+const IconSend = (props: React.ComponentProps<'svg'>) => ( <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M10 14l11 -11" /><path d="M21 3l-6.5 18a.55 .55 0 0 1-1 0l-3.5-7l-7-3.5a.55 .55 0 0 1 0-1l18-6.5" /></svg> );
 // ------------------
 
 interface Message {
@@ -27,10 +27,12 @@ export function ChatPage() {
   const animatedTitle = useTextScramble('Agente JV - Atendimento Virtual');
 
   const scrollToBottom = () => {
+    // Rola a área de scroll para o final
     viewport.current?.scrollTo({ top: viewport.current.scrollHeight, behavior: 'smooth' });
   };
 
   useEffect(() => {
+    // Sempre que as mensagens mudam, rola para baixo
     setTimeout(scrollToBottom, 100);
   }, [messages]);
 
