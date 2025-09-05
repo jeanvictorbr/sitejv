be3faf04914261c591e9bf4c3a65adfaf6ef5bb1
jeanvictorbr <jeanvictorbr@gmail.com>
2025-09-05 03:12:28 -0300

    Update HackerPlaceholder.tsx

--- src/components/HackerPlaceholder.tsx
+++ src/components/HackerPlaceholder.tsx
@@ -4,19 +4,10 @@ import classes from './HackerPlaceholder.module.css';
 
 export function HackerPlaceholder() {
   const animatedText = useTextScramble('JV STORE');
-
   return (
     <div className={classes.placeholderContainer}>
-      <div className={classes.rays}>
-        <div className={classes.ray}></div>
-        <div className={classes.ray}></div>
-        <div className={classes.ray}></div>
-        <div className={classes.ray}></div>
-      </div>
-      <Text
-        className={classes.hackerText}
-        dangerouslySetInnerHTML={{ __html: animatedText }}
-      />
+      <div className={classes.rays} />
+      <Text className={classes.hackerText} dangerouslySetInnerHTML={{ __html: animatedText }} />
     </div>
   );
 }
