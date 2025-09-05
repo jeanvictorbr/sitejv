67e226938e0c839ed76a113af92b6800586db5bb
jeanvictorbr <jeanvictorbr@gmail.com>
2025-09-05 02:28:58 -0300

    Update NewsCard.tsx

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import classes from './NewsCard.module.css';
 import { HackerPlaceholder } from './HackerPlaceholder'; 
 
-// ÍCONE SVG ADICIONADO DIRETAMENTE NO CÓDIGO
+// ÍCONE SVG ADICIONADO DIRETAMENTE NO CÓDIGOs
 const IconCalendar = (props: React.ComponentProps<'svg'>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
     <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
